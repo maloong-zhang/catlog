c8dff86ad01b77a17dc12a311b9b459ccad79878
maloong <maloong2022@gmail.com>
2024-06-22 16:12:06 +0400

    feat: first verion
    finished the first verion for catlog, setting log level, formatter etc

---
+++ example/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/zerotone.ae/catlog"
+)
+
+func main() {
+	catlog.Info("std log")
+	catlog.SetOptions(catlog.WithLevel(catlog.DebugLevel))
+	catlog.Debug("change std log to debug level")
+	catlog.SetOptions(catlog.WithFormatter(&catlog.JsonFormatter{IgnoreBasicFields: false}))
+	catlog.Debug("log in json format")
+	catlog.Info("another log in json format")
+
+	// output to file
+	fd, err := os.OpenFile("test.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalln("create file text.log failed")
+	}
+	defer fd.Close()
+
+	l := catlog.New(catlog.WithLevel(catlog.InfoLevel), catlog.WithOutput(fd), catlog.WithFormatter(&catlog.JsonFormatter{IgnoreBasicFields: false}))
+	l.Info("custom log with json formatter")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/zerotone.ae/catlog
 
 go 1.22.4
+
+require github.com/json-iterator/go v1.1.12
+
+require (
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+)
